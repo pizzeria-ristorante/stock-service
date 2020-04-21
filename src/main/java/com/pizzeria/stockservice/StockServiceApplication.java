24769ba3a97ced41b672665fde2c5ee4b087c324
joachimprinzbach <joachim.prinzbach@gmail.com>
2020-04-21 22:12:38 +0200

    FIX PACKAGE

--- src/main/java/com/pizzeria/stockservice/StockServiceApplication.java
+++ src/main/java/com/pizzeria/stockservice/StockServiceApplication.java
@@ -1,4 +1,4 @@
-package com.pizzeria.ristorante;
+package com.pizzeria.stockservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
