9d324c7b8ee8dbe025176084eaf70f0d1e42ec9d
joachimprinzbach <joachim.prinzbach@gmail.com>
2020-04-22 23:29:04 +0200

    We ned more stuff!

--- src/main/resources/db/local/afterMigrate.sql
+++ src/main/resources/db/local/afterMigrate.sql
@@ -1,20 +1,20 @@
 INSERT INTO INGREDIENT
     (id, name, quantity)
 VALUES
-    (1, 'Pizzateig', 114),
-    (2, 'Tomaten', 45),
-    (3, 'Mozzarella', 61),
-    (4, 'Salami', 33),
-    (5, 'Pilze', 45),
-    (6, 'Schinken', 26),
-    (7, 'Steinpilze', 11),
-    (8, 'Gorgonzola', 22),
-    (9, 'Ananas', 3),
-    (10, 'Sardellen', 4),
-    (11, 'Knoblauch', 5),
-    (12, 'Oel', 955),
-    (13, 'Thunfisch', 10),
-    (14, 'Zwiebeln', 66),
-    (15, 'scharfe Salami', 51),
-    (16, 'Bueffelmozzarella', 21)
+    (1, 'Pizzateig', 3568),
+    (2, 'Tomaten', 5000),
+    (3, 'Mozzarella', 4144),
+    (4, 'Salami', 1211),
+    (5, 'Pilze', 845),
+    (6, 'Schinken', 826),
+    (7, 'Steinpilze', 545),
+    (8, 'Gorgonzola', 363),
+    (9, 'Ananas', 22),
+    (10, 'Sardellen', 256),
+    (11, 'Knoblauch', 580),
+    (12, 'Oel', 1699),
+    (13, 'Thunfisch', 544),
+    (14, 'Zwiebeln', 2771),
+    (15, 'scharfe Salami', 869),
+    (16, 'Bueffelmozzarella', 350)
     ;
