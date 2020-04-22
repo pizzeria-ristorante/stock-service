5addc59a34dec031a2c8cbc5a647872619ed3c33
joachimprinzbach <joachim.prinzbach@gmail.com>
2020-04-22 11:48:18 +0200

    Add swagger and some log statements

--- src/main/java/com/pizzeria/stockservice/application/StockServiceRestController.java
+++ src/main/java/com/pizzeria/stockservice/application/StockServiceRestController.java
@@ -3,6 +3,7 @@ package com.pizzeria.stockservice.application;
 import com.pizzeria.stockservice.domain.Ingredient;
 import com.pizzeria.stockservice.domain.IngredientStockService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,15 +15,18 @@ import java.util.stream.Collectors;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "api/ingredients")
+@Slf4j
 public class StockServiceRestController {
 
     private final IngredientStockService ingredientStockService;
 
     @GetMapping
     public Set<IngredientDto> getAllIngredientsInStock(@RequestParam Optional<String> name) {
+        log.info("Getting all ingredients.");
         Iterable<Ingredient> ingredients = ingredientStockService.getAllIngredientsInStock();
         Set<Ingredient> allIngredients = new HashSet<>();
         ingredients.forEach(allIngredients::add);
+        log.info("Found total of {} ingredient entries.", allIngredients.size());
         return allIngredients
                 .stream()
                 .filter(ingredient -> name.isPresent() ? ingredient.getName().equals(name.get()) : true)
@@ -32,6 +36,7 @@ public class StockServiceRestController {
 
     @GetMapping("/{id}")
     public ResponseEntity<IngredientDto> getbyId(@PathVariable("id") int id) {
+        log.info("Getting ingredient with id: {}", id);
         return ingredientStockService.getIngredientById(id)
                 .map(ingredient -> ResponseEntity.ok().body(this.mapToDto(ingredient)))
                 .orElseGet(() -> ResponseEntity.notFound().build());
